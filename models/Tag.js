d4b590f707a92b98702257ae5e766a5edfa8b83d
KimboSlice <kimberlyhoward529@gmail.com>
2022-07-27 21:45:44 -0400

    Connected sequelize

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,26 @@
+// Bring in sequelize
 const { Model, DataTypes } = require('sequelize');
 
+// connect to sequelize
 const sequelize = require('../config/connection.js');
 
+// Create Tag Model
 class Tag extends Model {}
 
+// Specify what inputs can be in Tag table
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+// Import express and routes
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
 
+// import sequelize connection
+const sequelize = require('./config/connection');
+
+// initialize app to be express and set port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Get middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// use routes data
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
